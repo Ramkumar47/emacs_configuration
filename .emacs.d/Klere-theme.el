caffedf69a53e6be1f58ef072637443ca0e909eb
Ramkumar47 <ramkumar21194@gmail.com>
2019-05-18 20:10:19 +0530

    added initial configuration files

---
+++ .emacs.d/Klere-theme.el
@@ -0,0 +1,8 @@
+(deftheme Klere
+  "Created 2018-07-08.")
+
+(custom-theme-set-variables
+ 'Klere
+ '(custom-safe-themes (quote ("1884b12dbfa952664f1119bd6d89aaee77b35fbe3c3539381fbdc84a5bd907ef" "5fcc2d6be64f80162cc1b987454748b82464b490cb16b017daa1499d23156904" default))))
+
+(provide-theme 'Klere)
---
+++ .emacs.d/ramkumar-theme.el
@@ -0,0 +1,8 @@
+(deftheme ramkumar
+  "Created 2018-07-07.")
+
+(custom-theme-set-variables
+ 'ramkumar
+ '(custom-safe-themes (quote ("1884b12dbfa952664f1119bd6d89aaee77b35fbe3c3539381fbdc84a5bd907ef" default))))
+
+(provide-theme 'ramkumar)
